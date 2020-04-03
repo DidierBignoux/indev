f60fb3ff6e04453efdb8f32a5cbed57152ca1eb3
DidierBignoux <didieransleybignoux@live.com>
2020-04-03 16:01:44 +0400

    add fontAwesome

--- src/componets/Footer/footer.js
+++ src/componets/Footer/footer.js
@@ -1,13 +1,40 @@
-import React from 'react';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Footer() {
   return (
-    <div>
-        Footer
-        
-    </div>
+    <footer className="footer-area">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-12">
+            <div className="footer-top flex-column">
+              <div className="footer-logo">
+                <Link href="#">
+                  <img src={require("../../Assets/img/logo.png")} alt="" />
+                </Link>
+                <h4>Follow Me</h4>
+              </div>
+              {/* <div className="footer-social">
+							<Link href="#"><i className="fa fa-facebook"></i><Link />
+							<Link href="#"><i className="fa fa-twitter"></i><Link />
+							<Link href="#"><i className="fa fa-dribbble"></i><Link />
+							<Link href="#"><i className="fa fa-behance"></i><Link />
+						</div> */}
+              <div className="footer-social">
+              <FontAwesomeIcon icon="coffee" />
+
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="row footer-bottom justify-content-center">
+				<p className="col-lg-8 col-sm-12 footer-text">					
+Copyright  All rights reserved | This template is made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <Link href="https://colorlib.com" target="_blank">Colorlib<Link />
+</p>
+			</div> */}
+      </div>
+    </footer>
   );
 }
 
